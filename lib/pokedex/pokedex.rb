71b72069613b55eb0e02747821a327cb5e5bbe30
Diogo Kamioka <dkamioka@paula-kamiokas-imac.local>
2014-04-17 04:46:23 -0300

    Adding some test

--- lib/pokedex/pokedex.rb
+++ lib/pokedex/pokedex.rb
@@ -1,4 +1,6 @@
 module Pokeapi
   class Pokedex
+    include HTTParty
+    base_uri = 'http://pokeapi.co/api/v1/'
   end
 end
--- spec/lib/pokedex/pokedex_spec.rb
+++ spec/lib/pokedex/pokedex_spec.rb
@@ -2,7 +2,17 @@ require_relative '../../spec_helper'
 # For Ruby < 1.9.3, use this instead of require_relative
 # require (File.expand_path('./../../../spec_helper', __FILE__))
  
-describe Dish::Player do
+describe Pokeapi::Pokedex do
+	
+  describe "default attributes" do
+    it "must include httparty methods" do
+      Pokeapi::Pokedex.must_include HTTParty
+    end
+
+    it "must have the base url set to the Pokeapi endpoint" do
+      Pokeapi::Pokedex.base_uri.must_equal 'http://pokeapi.co/api/v1/'
+    end
+  end
  
   it "must work" do
     "Yay!".must_be_instance_of String
